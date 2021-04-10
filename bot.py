557eccb403a6885df6f4b2c3f3d5767afecd2659
99atran <99atran@gmail.com>
2021-04-10 14:53:18 -0400

    broke up functions for more versatile use

--- bot.py
+++ bot.py
@@ -14,8 +14,34 @@ command_prefix = '$'
 bot = commands.Bot(command_prefix=command_prefix)
 
 # FUNCTION DEFINTIONS
+async def join_call(ctx):
+    for vc in ctx.guild.voice_channels:
+        for member in vc.members:
+            if ctx.message.author.name == member.name:
+                try:
+                    await vc.connect()
+                    return True, vc
+                except discord.ClientException:
+                    for v_client in bot.voice_clients:
+                        if v_client.guild == ctx.message.guild:
+                            await v_client.move_to(vc)
+                    return True, vc
+                except:
+                    print('uh oh')
+                return False, NULL
+    await ctx.send(ctx.message.author.name + ' isn\'nt in a voice channel :(')
+    return False, NULL
 
-
+async def disconnect_call(ctx):
+    for v_client in bot.voice_clients:
+        if v_client.guild == ctx.message.guild:
+            vc_name = v_client.channel.name
+            await v_client.disconnect()
+            await ctx.send('Disconnected from the ' + vc_name + ' channel')
+            
+async def play_audio(ctx):
+    return
+            
 # EVENTS
 @bot.event
 async def on_connect():
@@ -27,35 +53,26 @@ async def on_connect():
         
 # COMMANDS
 @bot.command(name='join_call')
-async def join_call(ctx):
-    for vc in ctx.guild.voice_channels:
-        for member in vc.members:
-            if ctx.message.author.name == member.name:
-                try:
-                    await vc.connect()
-                    await ctx.send('done')
-                except discord.ClientException:
-                    for v_client in bot.voice_clients:
-                        if v_client.guild == ctx.message.guild:
-                            await v_client.move_to(vc)
-                            await ctx.send('done')
-                except:
-                    print('uh oh')
-            break
+async def join(ctx):
+    await join_call(ctx)
     
 @bot.command(name='disconnect')
 async def disconnect(ctx):
-    for v_client in bot.voice_clients:
-        if v_client.guild == ctx.message.guild:
-            vc_name = v_client.channel.name
-            await v_client.disconnect()
-            await ctx.send('Disconnected from the ' + vc_name + ' channel')
+    await disconnect_call(ctx)
             
 @bot.command(name='bakamitai')
+async def bakamitai(ctx):
+    in_call, vc = await join_call(ctx)
+    if in_call:
+        for v_client in bot.voice_clients:
+            if v_client.channel.id == vc.id:
+                await ctx.send('yay')
+                return     
 
 
 @bot.command(name='shutdown')
 async def shutdown(ctx):
+    await disconnect_call(ctx)
     await ctx.send('BYE BYE')
     await bot.close()
 
